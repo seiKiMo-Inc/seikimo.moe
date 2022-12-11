7595c503e2920bde7fb351b1253d9bc312af2d60
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-12-11 12:39:15 +0530

    Customize and setup project structure

---
+++ src/ui/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../css/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World lol</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import tsconfigPaths from "vite-tsconfig-paths"
+import * as path from 'path'
 
 export default defineConfig({
-    plugins: [reactRefresh()]
+    plugins: [reactRefresh(), tsconfigPaths()],
+    resolve: {
+        alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    }
 })
