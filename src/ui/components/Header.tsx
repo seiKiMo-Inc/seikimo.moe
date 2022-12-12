69fddd149ce7de22347d5b028a3c95a1427d9a6c
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-12-12 10:45:07 +0530

    Add scroll button

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -5,12 +5,18 @@ import { spawnSnow, spawnSnowCSS } from "@utils/pureSnow";
 import "@css/Header.css";
 
 class Header extends React.Component {
+    scrollToNextSection = () => {
+        (document.getElementsByClassName("amogus")[0] as HTMLDivElement).scrollIntoView({
+            behavior: "smooth",
+        })
+    }
+
     render() {
         return (
             <div
                 className="Header"
                 onLoad={() => {
-                    spawnSnow(200, (document.getElementById("snow") as HTMLDivElement));
+                    spawnSnow(200, document.getElementById("snow") as HTMLDivElement);
                     spawnSnowCSS();
                 }}
             >
@@ -23,6 +29,13 @@ class Header extends React.Component {
                     <div className="HeaderByline">
                         <p>Stupid Dumbasses Make Based Software</p>
                     </div>
+                </div>
+                <div className="HeaderScrollButton" onClick={this.scrollToNextSection}>
+                    <svg>
+                        <path className="ScrollArrow1" d="M0 0 L30 32 L60 0"></path>
+                        <path className="ScrollArrow2" d="M0 20 L30 52 L60 20"></path>
+                        <path className="ScrollArrow3" d="M0 40 L30 72 L60 40"></path>
+                    </svg>
                 </div>
             </div>
         );
