bce017648a64504435d74ea67100d9d342441ea4
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-12-19 13:08:57 +0530

    Add organization section

---
+++ src/ui/components/OrgInfo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import OrgMembers from "@components/common/OrgMembers";
+
+import "@css/OrgInfo.css";
+
+class OrgInfo extends React.Component {
+    render() {
+        return (
+            <div className="OrgInfo">
+                <div className="OrgShapeDivider">
+                    <img src="wave.svg" alt="wave" />
+                </div>
+                <div className="OrgInfoDescription">
+                    <p>
+                        Members
+                    </p>
+                </div>
+                <OrgMembers />
+            </div>
+        )
+    }
+}
+
+export default OrgInfo;
