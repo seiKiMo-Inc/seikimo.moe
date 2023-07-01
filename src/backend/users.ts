af36ab52c5c767a52d31a590abacfca8b701be53
KingRainbow44 <kobedo11@gmail.com>
2023-07-01 02:24:52 -0400

    Check if the app is running in a development environment

--- src/backend/users.ts
+++ src/backend/users.ts
@@ -1,7 +1,9 @@
 import { Member } from '@backend/types'
 
+const MEMBERS_URL = import.meta ? `http://localhost:3000/org` : `${window.location.href}org`;
+
 export async function getAllMembers(): Promise<Member[]> {
-    const response = await fetch(`${window.location.href}org`, {
+    const response = await fetch(MEMBERS_URL, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
