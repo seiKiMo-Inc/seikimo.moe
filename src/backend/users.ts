e7476a78233b3f01865281ad1bec31b855944cb5
KingRainbow44 <kobedo11@gmail.com>
2023-07-01 01:53:55 -0400

    Use the window's URL to fetch org members

--- src/backend/users.ts
+++ src/backend/users.ts
@@ -1,7 +1,7 @@
 import { Member } from '@backend/types'
 
 export async function getAllMembers(): Promise<Member[]> {
-    const response = await fetch(`https://api.arikatsu.lol/org`, {
+    const response = await fetch(`${window.location.href}org`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
