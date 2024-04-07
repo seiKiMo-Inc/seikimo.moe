5e9f043a455f8cd1a45f4456a9e659eeae9a1888
KingRainbow44 <kobedo11@gmail.com>
2024-04-07 18:14:31 -0400

    feat(account): Add page to delete your account
    i deleted my production account to test this.

--- src/ui/pages/DeleteAccount.tsx
+++ src/ui/pages/DeleteAccount.tsx
@@ -1,7 +1,50 @@
-function DeleteAccount() {
-    return (
-        <div className={"DeleteAccount"}>
+import { expectedOrigin, newCall } from "@app/index";
+import { useState } from "react";
 
+function DeleteAccount() {
+    const url = `${expectedOrigin()}/account/delete`;
+
+    const [status, setStatus] = useState<string | null>(null);
+
+    return (
+        <div className={"flex flex-col p-5"}>
+            <span>To delete your account, click the button below.</span>
+            { status && (
+                <span>{status}</span>
+            ) }
+            <span
+                className={"px-2 py-1 bg-blue-500 w-fit hover:cursor-pointer"}
+                onClick={async () => {
+                    // Check if the credentials are stored.
+                    const creds = localStorage.getItem("credentials");
+                    if (!creds) {
+                        window.open(`${expectedOrigin()}/login?redirect=${url}&app=Delete Account&handoff=true`);
+                        setStatus("Follow the link to delete your account.");
+                    } else try {
+                        // Parse the credentials.
+                        const { token } = JSON.parse(creds);
+                        // Attempt to delete the account.
+                        const response = await fetch(newCall("account"), {
+                            method: "DELETE",
+                            headers: { Authorization: token }
+                        });
+
+                        if (response.status == 200) {
+                            setStatus("Account deleted successfully.");
+
+                            // Remove the credentials.
+                            localStorage.removeItem("credentials");
+                        } else {
+                            setStatus("Unable to delete account Please contact us at support@seikimo.moe.");
+                        }
+                    } catch (error) {
+                        window.open(`${expectedOrigin()}/login?redirect=${url}&app=Delete Account&handoff=true`);
+                        setStatus("Follow the link to delete your account.");
+                    }
+                }}
+            >
+                Delete Account
+            </span>
         </div>
     );
 }
